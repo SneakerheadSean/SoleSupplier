4699523219d2a7964235fda8c4d770d578ef0701
Sean Hughes <sneakerheadsean@gmail.com>
2022-02-01 20:53:13 -0500

    Adding Heritage and Images

--- script.js
+++
@@ -1,8 +0,0 @@
-function openSneaker(sneakerName) {
-  var i;
-  var x = document.getElementsByClassName("sneaker");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  document.getElementById(sneakerName).style.display = "block";
-}
